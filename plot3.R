9cfd1702dab87d81535672e83497bf69d6bc3443
agrodowski <adam.grodowski@gmail.com>
2016-11-12 07:51:08 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,19 +1,16 @@
-# Exploratory Data Analysis course - Course Project 1
+# Exploratory Data Analysis couse - Course Project 1
 # Author: Adam Grodowski (adam.grodowski@gmail.com)
 library(data.table)
 
 PrepareHouseholdPowerData <- function(file, cols, filter) {
     # Extracts and refines the relevant observations and variables using fread 
-    # and a 'setAs' trick to compensate for fread bug #2660 which makes numeric 
-    # column processed as character due to '?' character found by fread during 
-    # scanning phase regardless of 'na.strings' value. Requires 'data.table'.
+    # optimised to retrieve selected columns and rows only. Requires 'data.table'.
     #
     # Args:
     #   file: The file with household power consumption data.
-    #   cols: A character vector with variable types. Use 'mynumeric' instead 
-    #         of 'numeric' to treat '?' as NA in numeric columns. Use 'NULL' to 
+    #   cols: A character vector with variable types. Use 'NULL' to 
     #         skip that column. Must have 9 values, one for each column.
-    #   filter: A logical closure for filtering the data.
+    #   filter: A logical closure for filtering the data rows.
     #
     # Returns:
     #   A data.table object with initially selected variables that satisfy 
@@ -21,9 +18,7 @@ PrepareHouseholdPowerData <- function(file, cols, filter) {
     #   (+) DateTime: a POSIXct object computed from Date and Time variables 
     #                 in %d/%m/%Y %H:%M:%S format.
     #   (-) Date, Time: as not needed anymore
-    setClass("mynumeric")
-    setAs("character","mynumeric", 
-          function(from) if (from=='?') NA else as.numeric(from))
+    
     # Error handling
     if (length(cols) != 9)
         stop("There must be 9 values provided in cols, one for each column.
@@ -38,18 +33,19 @@ PrepareHouseholdPowerData <- function(file, cols, filter) {
     datawant[, c("Date","Time") := NULL]  # drops variables not needed anymore
 }
 
-
 # Retrieve dataset (if not present)
 file <- "household_power_consumption.txt"
 if (!file.exists(file)) {
-  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","household_power_consumption.zip",method="curl")
-  unzip("household_power_consumption.zip")
+    download.file(
+        "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip",
+        "household_power_consumption.zip",method="curl")
+    unzip("household_power_consumption.zip")
 }
 
 # Prepare column types and filter closure for data loading
 cols <- rep("NULL",9)  # initially nullify all columns
 cols[1:2] <- "character"  # Date & Time, it is faster to read as chars
-cols[7:9] <- "mynumeric"  # class trick to prevent #2660
+cols[7:9] <- "numeric"
 filter <- function(dt) 
     dt[Date %in% c("1/2/2007","2/2/2007")] # only rows wihtin 1-2/2/2007
 # Extract only relevant and needed data
